7cfeb894a337471901e66baf969a8fd702f49f67
Manaswini Nadiminty <nadiminty.manaswini@gmail.com>
2026-07-18 10:14:43 +0530

    Updated with photo upload and ui /ux

--- static/script.js
+++ static/script.js
@@ -13,7 +13,7 @@ async function submitComplaint() {
     formData.append("category", category);
     if (photo) formData.append("photo", photo);
 
-    await fetch("/add_complaints", {
+    await fetch("/add_complaint", {
         method: "POST",
         body: formData
     });
